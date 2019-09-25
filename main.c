e1e3a8739526f77dc1316470fc3f2221fc39d363
Ethan Malin <ethanmalin@Ethans-MacBook-Pro-2.local>
2019-09-25 18:38:25 -0500

    cleaned up and made quit command work

--- main.c
+++ main.c
@@ -1,13 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
+
+const char *EXIT = "q";
 
 int main(int argc, char **argv) {
-  char *name = malloc(256);
-  printf("What is your name?\n");
-  fgets(name, 256, stdin);
-  printf("Nice to meet you %s\n", name);
-  free(name);
+  bool quit = false;
+  char *input = malloc(256);
+  
+  while (!quit) {
+    // prompt
+    printf("\n >");
+    fflush(stdout);
+
+    // get & strip input
+    fgets(input, 256, stdin);
+    strtok(input, "\n");  // gets rid of trailing newline from fgets input
+
+    // handle command
+    if (strcmp(EXIT, input) == 0) {
+      printf("Goodbye.\n");
+      quit = true;
+    } else {
+      printf("You entered: %s with length %lu", input, strlen(input));
+    }
+
+  }
+
+  // deallocate resources
+  free(input);
   return 0;
 }
    
