04a13502eca97eaedb871e7bea98693ada9581af
Ethan Malin <ethanmalin@Ethans-MacBook-Pro-2.local>
2019-09-27 19:31:10 -0500

    start of InputBlock list structure setup

--- main.c
+++ main.c
@@ -9,6 +9,8 @@ const int MAX_PIPELINE_LENGTH = 32; // also arbitrary
 const int MAX_INPUT_BLOCK_LENGTH = 256; // ... you guessed it
 
 struct InputBlock {
+  struct InputBlock *prev;
+  struct InputBlock *next;
   char *execName;
   // WARNING -- if files unused they MUST explicitly be set to NULL
   char *inputFile; 
@@ -38,19 +40,18 @@ char** split(char *input, char *delim) {
   char **res;
   res = malloc(MAX_PIPELINE_LENGTH * sizeof(char*));
   
-  for(int i = 0; i < MAX_PIPELINE_LENGTH; i++) { res[i] = NULL; } // initialize to NULL, necessary?
+  for(int i = 0; i < MAX_PIPELINE_LENGTH; i++) { res[i] = NULL; } // initialize to NULL
   char *ptr = strtok(input, delim); // feed strtok and get first ptr
   int i = 0;
   while (ptr != NULL) {
     // if we have gotten too many inputs, free and abort, returning NULL
-    if (i > MAX_PIPELINE_LENGTH - 1) {
+    if (i == MAX_PIPELINE_LENGTH) {
       printf("Pipeline too long! Aborting...\n");
       for (int i = 0; i < MAX_PIPELINE_LENGTH; i++) { free(res[i]); }
       free(res);
       return NULL;
     }
     // append to res
-    printf("Adding to res: %s\n", ptr);
     res[i] = malloc(MAX_INPUT_BLOCK_LENGTH);
     strcpy(res[i], ptr);
     i++;
@@ -64,6 +65,7 @@ int main(int argc, char **argv) {
   bool quit = false;
   char *input = malloc(MAX_INPUT_LENGTH);
   char **inputPipeSplit;
+  
   while (!quit) {
     // prompt
     printf("[activeDirectory]-->");
@@ -71,17 +73,8 @@ int main(int argc, char **argv) {
 
     // get & strip input
     fgets(input, MAX_INPUT_LENGTH, stdin);
-    strtok(input, "\n");  // gets rid of trailing newline from fgets input
+    strtok(input, "\n");  // gets rid of trailing (first!) newline from fgets input
 
-    inputPipeSplit = split(input, "|");
-    for(int i = 0; i < MAX_PIPELINE_LENGTH; i++) {
-      if (inputPipeSplit[i] == NULL) {
-	break;
-      } else {
-	printf("block %d: %s\n", i, inputPipeSplit[i]);
-      }
-    }
-    
     // handle command
     if (strcmp(EXIT, input) == 0) {
       printf("Goodbye.\n");
@@ -90,6 +83,59 @@ int main(int argc, char **argv) {
       printf("You entered: %s with length %lu\n", input, strlen(input));
     }
 
+    // make InputBlocks linked list
+    // split and debug output input
+    inputPipeSplit = split(input, "|");
+    for(int i = 0; i < MAX_PIPELINE_LENGTH; i++) {
+      if (inputPipeSplit[i] == NULL) {
+	break;
+      } else {
+	printf("block %d: %s\n", i, inputPipeSplit[i]);
+      }
+    }
+
+    // persistent pointer to the first element in the list
+    struct InputBlock *first = malloc(sizeof(struct InputBlock));
+    first->prev = NULL;
+    first->next = NULL;
+
+    // pointers for initializing the list (bookkeeping pointers)
+    struct InputBlock *prev = NULL;
+    struct InputBlock *current = first;
+    for(int i = 0; i < MAX_PIPELINE_LENGTH; i++) {
+      if(inputPipeSplit[i] == NULL) {
+	// prev->next already initialized to NULL, just break
+	break;
+      }
+
+      // allocate space and hook list up
+      current = malloc(sizeof(struct InputBlock));      
+      if (prev != NULL) { prev->next = current; }
+      current->next = NULL;
+      current->prev = prev;
+      
+      // TODO need functions to break inputPipeSplit[i] into string fields 
+      // something like..
+      /*
+	current->execName = parseInputBlockExecName(inputPipeSplit[i]);
+	current->inputFile = parseInputBlockInputFile(inputPipeSplit[i]);
+	current->outputFile = parseInputBlockOutputFile(inputPipeSplit[i]);
+	current->argc = parseInputBlockArgs(inputPipeSplit[i], current->args);
+      */
+
+      // set up for iteration
+      prev = current;
+
+    }
+
+    // free InputBlocks -- should declare and free these outside while loop, but careful of unattached blocks after loop iteration!!
+    struct InputBlock *eraser = first;
+    while (eraser->next != NULL) {
+      eraser = eraser->next;
+      free(eraser->prev);
+    }
+    free(eraser);
+    
   }
 
   // deallocate resources
