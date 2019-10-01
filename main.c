a8c75d2bb4d216c411420a6a2ee134b9d19a228e
Ethan Malin <ethanmalin@Ethans-MacBook-Pro-2.local>
2019-09-30 19:03:21 -0500

    put tests in their own function, uncommented main

--- main.c
+++ main.c
@@ -13,9 +13,69 @@ const int MAX_INPUT_BLOCK_LENGTH = 256;
 const int MAX_PIPELINE_LENGTH = 32;
 // ----------
 
+void test();
+
 int main(int argc, char **argv) {
   char *input = malloc(MAX_INPUT_LENGTH);
 
+  // freed per iteration
+  char **inputPipeSplit; // array of strings
+  struct InputBlock *first;
+  
+  while (true) {
+    // replace with actual active directory
+    printf("[activeDirectory]-->");
+    fflush(stdout);
+
+    // get & strip input
+    fgets(input, MAX_INPUT_LENGTH, stdin);
+    strtok(input, "\n");  // gets rid of trailing (first!) newline from fgets input
+
+    // handle command -- eventually needs to be a branch?
+    if (strcmp(EXIT, input) == 0) {
+      printf("Goodbye.\n");
+      break; // haven't allocated anything this iteration, safe to exit
+    }
+
+    // make InputBlocks linked list, array of size MAX_PIPELINE_LENGTH, padded with NULL at the end
+    inputPipeSplit = split(input, "|", MAX_PIPELINE_LENGTH);
+    if (inputPipeSplit == NULL) {
+      printf("Error splitting pipes\n");
+      continue;
+    }
+
+    createInputBlockLinkedList(&first, inputPipeSplit, MAX_PIPELINE_LENGTH, MAX_INPUT_BLOCK_LENGTH);
+    if (first == NULL) {
+      printf("ERROR: could not create linked list from strings.\n");
+      break;
+    }
+
+    while (first != NULL) {
+      printInputBlock(first);
+      first = first->next;
+    }
+    /*
+     * actually run the commands here
+     */
+    
+    // free inputPipeSplit every iteration
+    for (int j = 0; j < MAX_PIPELINE_LENGTH; j++) {
+      if (inputPipeSplit[j] == NULL) { break; }
+      free(inputPipeSplit[j]);
+    }
+    free(inputPipeSplit);
+
+    // free InputBlocks every iteration
+    freeInputBlockLinkedList(first);
+  }
+  
+  free(input);
+  return 0;
+}
+
+
+// TESTING FUNCTION
+void test() {
   // freed per iteration
   char **inputPipeSplit; // array of strings
   struct InputBlock *first;
@@ -50,51 +110,4 @@ int main(int argc, char **argv) {
 
     freeInputBlockLinkedList(first);
   }
-  
-  // while (true) {
-  //   // replace with actual active directory
-  //   printf("[activeDirectory]-->");
-  //   fflush(stdout);
-
-  //   // get & strip input
-  //   fgets(input, MAX_INPUT_LENGTH, stdin);
-  //   strtok(input, "\n");  // gets rid of trailing (first!) newline from fgets input
-
-  //   // handle command -- eventually needs to be a branch?
-  //   if (strcmp(EXIT, input) == 0) {
-  //     printf("Goodbye.\n");
-  //     break; // haven't allocated anything this iteration, safe to exit
-  //   }
-
-  //   // make InputBlocks linked list, array of size MAX_PIPELINE_LENGTH, padded with NULL at the end
-  //   inputPipeSplit = split(input, "|", MAX_PIPELINE_LENGTH);
-  //   if (inputPipeSplit == NULL) {
-  //     printf("Error splitting pipes\n");
-  //     continue;
-  //   }
-
-  //   // persistent pointer to the first element in the list
-  //   first = malloc(sizeof(struct InputBlock));
-  //   first->prev = NULL;
-  //   first->next = NULL;
-
-  //   createInputBlockLinkedList(first, inputPipeSplit, MAX_PIPELINE_LENGTH, MAX_INPUT_BLOCK_LENGTH);
-
-  //   /*
-  //    * actually run the commands here
-  //    */
-    
-  //   // free inputPipeSplit every iteration
-  //   for (int j = 0; j < MAX_PIPELINE_LENGTH; j++) {
-  //     if (inputPipeSplit[j] == NULL) { break; }
-  //     free(inputPipeSplit[j]);
-  //   }
-  //   free(inputPipeSplit);
-
-  //   // free InputBlocks every iteration
-  //   freeInputBlockLinkedList(first);
-  // }
-  
-  free(input);
-  return 0;
 }
